da983389cc9003e815a4f1e8029a714c48916eab
Chinmaya Upadhyay <chinmaya.upadhyay@softwebsolutions.com>
2023-01-24 16:10:39 +0530

    first commit
    first commit

---
+++ CICDWEB/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CICDWEB
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
